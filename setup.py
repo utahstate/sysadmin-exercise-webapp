17d58f32bb46e213bf026c2f06cea4e6c8b396fa
otigraoken <ken.andersen@usu.edu>
2024-09-25 17:22:15 -0600

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name="sysadminwebapp",
+    version="0.1",
+    description="Sysadmin web application",
+    author="Ken Andersen",
+    packages=['sysadminwebapp'],
+    classifiers=[
+        'Development Status :: Development',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ]
+)
